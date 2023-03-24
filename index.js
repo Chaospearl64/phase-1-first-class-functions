e5e102e675517293c0fd5a724fd706719dfcc199
Cody Cogbill <codeman24@gmail.com>
2023-03-24 14:19:25 -0600

    first class functions lab done

---
+++ index.js
@@ -0,0 +1,22 @@
+
+
+function receivesAFunction(callBack) {
+    callBack()
+};
+
+
+
+function returnsANamedFunction(){
+  fn = (function1) => {
+    
+  }
+  return fn
+};
+
+
+function returnsAnAnonymousFunction(){
+   return () => 1 + 1
+    }
+
+
+
